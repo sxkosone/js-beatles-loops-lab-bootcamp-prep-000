c131c0207fafc75adc86fe2f048fe74f45b84c3a
Susanna Kosonen <kosonen.susanna@gmail.com>
2018-04-10 15:42:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var newArray = [];
   for (var i = 0; i < musicians.length; i++) {
-    newArray[i] = musicians[i] + " plays " + instrument[i];
+    newArray[i] = musicians[i] + " plays " + instruments[i];
   }
   return newArray;
 }
